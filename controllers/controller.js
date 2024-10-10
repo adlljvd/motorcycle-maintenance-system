f2293aca40f2d5d6866115dd78115019b64bcd7a
adlljvd <adella.javd@gmail.com>
2024-10-10 14:08:02 +0700

    feat: mvc

---
+++ controllers/controller.js
@@ -0,0 +1,24 @@
+const {User, Motorcycle, Appointment, Service} = require('../models/index');
+
+class Controller {
+    static getRegisterForm(req, res) {
+        try{
+            // res.send('registerForm')
+            res.render('register-form.ejs')
+        }catch(error){
+            console.log(error)
+            res.send(error)
+        }
+    }
+
+    static postRegisterForm(req, res) {
+        try{
+            res.send('postRegisterfORM')
+        }catch(error){
+            console.log(error)
+            res.send(error)
+        }
+    }
+}
+
+module.exports = Controller
