c6444656b0a342840a22bab708521c7843edb2d4
Raffles <rfflsy16@gmail.com>
2024-10-10 16:45:26 +0700

    feat controller

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,13 +4,12 @@ class Controller {
 
     static async showLandingPage(req, res) {
         try {
-            
+            res.send('ini landing page')
         } catch (error) {
-
+            res.send(error)
+            console.log(error)
         }
     }
-    static getRegisterForm(req, res) {
-
     static async getRegisterForm(req, res) {
 
         try{
@@ -59,6 +58,69 @@ class Controller {
         }catch(error){
             console.log(error)
             res.send(error)
+        }
+    }
+    static async getAppointments(req, res) {
+        try {
+            
+        } catch (error) {
+            res.send(error)
+            console.log(error)
+        }
+    }
+    static async getAddAppointments(req, res) {
+        try {
+            res.render('addAppointments')
+        } catch (error) {
+            res.send(error)
+            console.log(error)
+        }
+    }
+    static async postAddAppointments(req, res) {
+        try {
+            const  {id} = req.params
+            const {brand, type, year, licensePlate, UserID, serviceName, description, price} =req.body
+            await Motorcycle.create({brand, type, year, licensePlate, UserID: id})
+            await Service.create({serviceName, description, price})
+
+            res.redirect('/appointments')
+        } catch (error) {
+            res.send(error)
+            console.log(error)
+        }
+    }
+    static async getAppointmentsEdit(req, res) {
+        try {
+            const { id } = req.params
+            const {brand, type, year, licensePlate, UserID, serviceName, description, price} =req.body
+
+        } catch (error) {
+            res.send(error)
+            console.log(error)
+        }
+    }
+    static async postAppointmentsEdit(req, res) {
+        try {
+            
+        } catch (error) {
+            res.send(error)
+            console.log(error)
+        }
+    }
+    static async deleteAppointments(req, res) {
+        try {
+            
+        } catch (error) {
+            res.send(error)
+            console.log(error)
+        }
+    }
+    static async getResultAppointments(req, res) {
+        try {
+            
+        } catch (error) {
+            res.send(error)
+            console.log(error)
         }
     }
 }
--- routers/index.js
+++ routers/index.js
@@ -6,14 +6,17 @@ const Controller = require('../controllers/controller');
 router.get('/', Controller.showLandingPage) // show all our services
 router.get('/register', Controller.getRegisterForm) // ini user ketika click navbar appointment
 router.post('/register', Controller.postRegisterForm)
-
-router.get('/appointments', Controller.getAppointments) //appointments list
-router.get('/appointments/add', Controller.getAddAppointments) // page form untuk user isi data diri dan motor
-router.post('/appointments/add', Controller.postAddAppointments)
-router.get('/appointments/:id/edit', Controller.getAppointmentsEdit) // form untuk mengedit sesuai id yang di click
-
 router.get('/login', Controller.getLoginForm)
 router.post('/login', Controller.postLoginForm)
+
+router.get('/appointments', Controller.getAppointments) //appointments list
+router.get('/appointments/:id/add', Controller.getAddAppointments) // page form untuk user isi data diri dan motor
+router.post('/appointments/:id/add', Controller.postAddAppointments)
+router.get('/appointments/:id/edit', Controller.getAppointmentsEdit) // form untuk mengedit sesuai id yang di click
+router.post('/appointments/:id/edit', Controller.postAppointmentsEdit) 
+router.get('/appointments/:id/delete', Controller.deleteAppointments) // delete/cancel id yang di inginkan oleh customer
+router.get('/appointments/:id/result', Controller.getResultAppointments) // menampilkan hasil appointment id customer
+
 
 
 module.exports = router
